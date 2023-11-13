2232b15baf35bb146b2abcce7d73a073e0e483eb
Thanawat Jarusuthirug <lynniswaifu@gmail.com>
2023-11-13 17:43:27 +0700

    fix bugs and add scoring

---
+++ elasticsearch_loader.py
@@ -0,0 +1,93 @@
+# py .\elasticsearch_loader.py --file .\Datasource\out.csv --index lyrics
+
+from elasticsearch import Elasticsearch, helpers
+import ndjson
+import argparse
+import uuid
+import csv
+
+es = Elasticsearch( "https://localhost:9200", basic_auth=("elastic", "262545"), verify_certs=False)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', help='Path to the .csv file', required=True)
+parser.add_argument('--index', help='The name of the Elasticsearch index', required=True)
+args = parser.parse_args()
+
+index = args.index
+file_path = args.file
+
+def csv_data(csv_file, _index):
+    with open(csv_file, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            yield {
+                "_index": _index,
+                "_id": uuid.uuid4(),
+                "_source": row
+            }
+
+def setting():
+    # Close the index
+    es.indices.close(index=index)
+
+    # Set index settings
+    settings_body = {
+        "settings": {
+            "index": {
+                "similarity": {
+                    "default": {
+                        "type": "BM25",
+                        "b": 0.75,
+                        "k1": 1.2
+                    }
+                },
+                "analysis": {
+                    "analyzer": {
+                        "lyrics_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "standard",
+                            "filter": [
+                                "lowercase",
+                                "asciifolding",
+                                "english_possessive_stemmer",
+                                "english_stop",
+                                "english_stemmer"
+                            ]
+                        }
+                    },
+                    "filter": {
+                        "english_possessive_stemmer": {
+                            "type": "stemmer",
+                            "language": "possessive_english"
+                        },
+                        "english_stop": {
+                            "type": "stop",
+                            "stopwords": "_english_"
+                        },
+                        "english_stemmer": {
+                            "type": "stemmer",
+                            "language": "english"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    es.indices.put_settings(index=index, settings=settings_body)
+
+    # Open the index
+    es.indices.open(index=index)
+
+def delete_index():
+    es.indices.delete(index=index, ignore=[400, 404])
+
+
+try:
+    delete_index()
+    response = helpers.bulk(es, csv_data(file_path, index))
+    setting()
+    # delete_index()
+    # print("\nRESPONSE:", response)
+except Exception as e:
+    print("\nERROR:", e)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, request
 from markupsafe import escape
 from flask import render_template
 from elasticsearch import Elasticsearch
+from flask import jsonify
 import math
-
 ELASTIC_PASSWORD = "262545"
 
 es = Elasticsearch("https://localhost:9200",
@@ -38,12 +38,63 @@ def search():
     }
     res = es.search(index='lyrics', body=body)
     hits = [{'title': doc['_source']['title'],
-            'lyrics': (doc['_source']['lyrics'][:500] + "...." if len(doc['_source']['lyrics']) > 500 else doc['_source']['lyrics']),
+            'lyrics': (doc['_source']['lyrics'][:500] + "...." if len(doc['_source']['lyrics']) > 500 else doc['_source']['lyrics']), 'id': doc['_source']['id'],
               'image_url': doc['_source']['image_url']}
             for doc in res['hits']['hits']]
     page_total = math.ceil(res['hits']['total']['value']/page_size)
     return render_template('search.html', keyword=keyword, hits=hits, page_no=page_no, page_total=page_total)
 
+
+@app.route('/song/<song_id>')
+def song(song_id):
+    body = {
+        "query": {
+        "term" : {
+                "id": song_id
+                }
+        }
+    }
+
+    res = es.search(index='lyrics', body=body)
+
+    for doc in res['hits']['hits']:
+        hits = {'title': doc['_source']['title'],
+                'lyrics': doc['_source']['lyrics'],
+                'image_url': doc['_source']['image_url']}
+    
+    # return render_template('song.html', hits=hits)
+    return render_template('song.html', hits=hits)
+
+@app.route('/autocomplete')
+def autocomplete():
+    search_term = request.args.get('q', '')
+    body = {
+        "size": 5,
+        "query": {
+            "multi_match": {
+                "query": search_term,
+                "fields": ["title^5", "lyrics"],
+                "type": "phrase_prefix"
+            }
+        }
+    }
+    res = es.search(index='lyrics', body=body)
+    suggestions = [{'title': doc['_source']['title'],'image_url': doc['_source']['image_url'], 'lyrics': doc['_source']['lyrics'], 'id': doc['_source']['id']} for doc in res['hits']['hits']]
+    return jsonify(suggestions)
+
+@app.route('/top')
+def top():
+    return render_template('topsong.html')
+
+
+@app.route('/random')
+def random():
+    return render_template('randomsong.html')
+
+
+# @app.route('/song')
+# def song():
+#     return render_template('song.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
